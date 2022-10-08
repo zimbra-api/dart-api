cdeb27e70178879272452444fecb580d86ed0f28
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-10-08 18:45:50 +0700

    Add end session test
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ test/account/end_session_test.dart
@@ -0,0 +1,49 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:zimbra_api/src/account/message/end_session_envelope.dart';
+import 'package:zimbra_api/src/account/message/end_session_request.dart';
+import 'package:zimbra_api/src/account/message/end_session_response.dart';
+
+void main() {
+  final faker = Faker();
+
+  group('End session tests', (() {
+    test('End session request', (() {
+      final logoff = faker.randomGenerator.boolean();
+      final clearAllSoapSessions = faker.randomGenerator.boolean();
+      final excludeCurrentSession = faker.randomGenerator.boolean();
+      final sessionId = faker.guid.guid();
+
+      final request = EndSessionRequest(
+        logoff: logoff,
+        clearAllSoapSessions: clearAllSoapSessions,
+        excludeCurrentSession: excludeCurrentSession,
+        sessionId: sessionId,
+      );
+      expect(request.getEnvelope().toJson(), {
+        'Body': {
+          'EndSessionRequest': {
+            '_jsns': 'urn:zimbraAccount',
+            'logoff': logoff,
+            'all': clearAllSoapSessions,
+            'excludeCurrent': excludeCurrentSession,
+            'sessionId': sessionId,
+          }
+        },
+      });
+    }));
+
+    test('End session response', (() {
+      final json = {
+        'Body': {
+          'EndSessionResponse': {
+            '_jsns': 'urn:zimbraAccount',
+          }
+        }
+      };
+      final envelope = EndSessionEnvelope.fromJson(json);
+      expect(envelope.endSessionBody.endSessionResponse, isNotNull);
+      expect(envelope.endSessionBody.endSessionResponse, isA<EndSessionResponse>());
+    }));
+  }));
+}

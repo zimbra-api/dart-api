df25d8367b805f315da1245e2977fb7f1e14a62b
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-10-10 14:26:33 +0700

    Add get signatures test
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ test/account/get_signatures_test.dart
@@ -0,0 +1,62 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:zimbra_api/src/account/message/get_signatures_envelope.dart';
+import 'package:zimbra_api/src/account/message/get_signatures_request.dart';
+import 'package:zimbra_api/src/common/enum/content_type.dart';
+
+void main() {
+  final faker = Faker();
+
+  group('Get signatures tests', (() {
+    test('Get signatures request', (() {
+      final request = GetSignaturesRequest();
+
+      expect(request.getEnvelope().toJson(), {
+        'Body': {
+          'GetSignaturesRequest': {
+            '_jsns': 'urn:zimbraAccount',
+          }
+        },
+      });
+    }));
+
+    test('Get signatures response', (() {
+      final name = faker.lorem.word();
+      final id = faker.guid.guid();
+      final cid = faker.guid.guid();
+      final value = faker.lorem.word();
+
+      final json = {
+        'Body': {
+          'GetSignaturesResponse': {
+            '_jsns': 'urn:zimbraAccount',
+            'signature': [
+              {
+                'name': name,
+                'id': id,
+                'cid': cid,
+                'content': [
+                  {
+                    'type': ContentType.textHtml.name,
+                    '_content': value,
+                  },
+                ],
+              }
+            ],
+          }
+        }
+      };
+      final envelope = GetSignaturesEnvelope.fromJson(json);
+      final response = envelope.getSignaturesBody.getSignaturesResponse;
+      final signature = response!.signatures.first;
+
+      expect(signature.name, name);
+      expect(signature.id, id);
+      expect(signature.cid, cid);
+
+      final content = signature.contents.first;
+      expect(content.type, ContentType.textHtml);
+      expect(content.value, value);
+    }));
+  }));
+}

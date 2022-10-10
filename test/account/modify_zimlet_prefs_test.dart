8d99b7ba30ce317165fc4d5dbbc9d20a490dd00d
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-10-10 16:24:50 +0700

    Add modify zimlet prefs test
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ test/account/modify_zimlet_prefs_test.dart
@@ -0,0 +1,49 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:zimbra_api/src/account/message/modify_zimlet_prefs_envelope.dart';
+import 'package:zimbra_api/src/account/message/modify_zimlet_prefs_request.dart';
+import 'package:zimbra_api/src/account/type/modify_zimlet_prefs_spec.dart';
+import 'package:zimbra_api/src/common/enum/zimlet_status.dart';
+
+void main() {
+  final faker = Faker();
+
+  group('Modify zimlet prefs tests', (() {
+    test('Modify zimlet prefs request', (() {
+      final name = faker.lorem.word();
+      final request = ModifyZimletPrefsRequest(zimlets: [ModifyZimletPrefsSpec(name, ZimletStatus.enabled)]);
+      expect(request.getEnvelope().toJson(), {
+        'Body': {
+          'ModifyZimletPrefsRequest': {
+            '_jsns': 'urn:zimbraAccount',
+            'zimlet': [
+              {
+                'name': name,
+                'presence': ZimletStatus.enabled.name,
+              }
+            ]
+          }
+        }
+      });
+    }));
+
+    test('Modify zimlet prefs response', (() {
+      final zimlet1 = faker.lorem.word();
+      final zimlet2 = faker.lorem.word();
+      final json = {
+        'Body': {
+          'ModifyZimletPrefsResponse': {
+            '_jsns': 'urn:zimbraAccount',
+            'zimlet': [
+              {'_content': zimlet1},
+              {'_content': zimlet2},
+            ],
+          }
+        }
+      };
+      final envelope = ModifyZimletPrefsEnvelope.fromJson(json);
+      final response = envelope.modifyZimletPrefsBody.modifyZimletPrefsResponse!;
+      expect(response.zimlets, [zimlet1, zimlet2]);
+    }));
+  }));
+}

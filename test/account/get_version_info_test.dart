44ab1aee959d3daf76f6d8e76172a4d8a764ac9e
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-10-10 14:35:38 +0700

    Add get version info test
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ test/account/get_version_info_test.dart
@@ -0,0 +1,51 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:zimbra_api/src/account/message/get_version_info_envelope.dart';
+import 'package:zimbra_api/src/account/message/get_version_info_request.dart';
+
+void main() {
+  final faker = Faker();
+
+  group('Get version info tests', (() {
+    test(' Get version info request', (() {
+      final request = GetVersionInfoRequest();
+
+      expect(request.getEnvelope().toJson(), {
+        'Body': {
+          'GetVersionInfoRequest': {
+            '_jsns': 'urn:zimbraAccount',
+          }
+        },
+      });
+    }));
+
+    test(' Get version info request', (() {
+      final fullVersion = faker.lorem.word();
+      final release = faker.lorem.word();
+      final date = faker.date.dateTime().toString();
+      final host = faker.internet.ipv4Address();
+
+      final json = {
+        'Body': {
+          'GetVersionInfoResponse': {
+            '_jsns': 'urn:zimbraAccount',
+            'info': {
+              'version': fullVersion,
+              'release': release,
+              'buildDate': date,
+              'host': host,
+            },
+          }
+        }
+      };
+      final envelope = GetVersionInfoEnvelope.fromJson(json);
+      final response = envelope.getVersionInfoBody.getVersionInfoResponse;
+      final versionInfo = response!.versionInfo!;
+
+      expect(versionInfo.fullVersion, fullVersion);
+      expect(versionInfo.release, release);
+      expect(versionInfo.date, date);
+      expect(versionInfo.host, host);
+    }));
+  }));
+}

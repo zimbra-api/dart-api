d0796ae7c9ebd42773c28d8dd46c8b5ec5e2bdd1
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-10-14 09:12:13 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- lib/src/mail/message/add_task_invite_request.dart
+++ lib/src/mail/message/add_task_invite_request.dart
@@ -2,28 +2,16 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-import 'package:zimbra_api/src/common/enum/participation_status.dart';
 import 'package:zimbra_api/src/common/type/soap_envelope.dart';
 import 'package:zimbra_api/src/common/type/soap_header.dart';
-import 'package:zimbra_api/src/common/type/soap_request.dart';
-import 'package:zimbra_api/src/mail/type/msg.dart';
 
+import 'add_appointment_invite_request.dart';
 import 'add_task_invite_body.dart';
 import 'add_task_invite_envelope.dart';
 
 /// Add a task invite
-class AddTaskInviteRequest extends SoapRequest {
-  /// iCalendar PTST (Participation status)
-  /// Valid values: NE|AC|TE|DE|DG|CO|IN|WE|DF
-  /// Meanings:
-  /// "NE"eds-action, "TE"ntative, "AC"cept, "DE"clined, "DG" (delegated), "CO"mpleted (todo), "IN"-process (todo),
-  /// "WA"iting (custom value only for todo), "DF" (deferred; custom value only for todo)
-  final ParticipationStatus? partStat;
-
-  /// Message
-  final Msg? msg;
-
-  AddTaskInviteRequest({this.partStat, this.msg});
+class AddTaskInviteRequest extends AddAppointmentInviteRequest {
+  AddTaskInviteRequest({super.partStat, super.msg});
 
   @override
   SoapEnvelope getEnvelope({SoapHeader? header}) =>

44e7a230059b434b9292922756b5d475f4788310
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-10-14 10:48:34 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- lib/src/common/soap/api.dart
+++ lib/src/common/soap/api.dart
@@ -5,16 +5,19 @@
 import 'dart:convert';
 
 import 'package:zimbra_api/src/common/soap/client.dart';
+import 'package:zimbra_api/src/common/type/soap_header.dart';
 import 'package:zimbra_api/src/common/type/soap_request.dart';
 
 abstract class Api {
   final Client _soapClient;
 
+  SoapHeader? _requestHeader;
+
   Api(String serviceHost) : _soapClient = Client(serviceHost);
 
   Future<Map<String, dynamic>> invoke(SoapRequest request) {
     return _soapClient
-        .sendRequest(jsonEncode(request.getEnvelope().toJson()))
+        .sendRequest(jsonEncode(request.getEnvelope(header: _requestHeader).toJson()))
         .then((response) => jsonDecode(response.body).cast<Map<String, dynamic>>());
   }
 }

f7a199664e99be237d3a1f145439138467149dfc
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-10-31 09:01:02 +0700

    Add send verification code test
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ test/mail/send_verification_code_test.dart
@@ -0,0 +1,39 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:zimbra_api/src/mail/message/send_verification_code_envelope.dart';
+import 'package:zimbra_api/src/mail/message/send_verification_code_request.dart';
+import 'package:zimbra_api/src/mail/message/send_verification_code_response.dart';
+
+void main() {
+  final faker = Faker();
+
+  group('Send verification code tests', (() {
+    test('Send verification code request', (() {
+      final address = faker.internet.email();
+
+      final request = SendVerificationCodeRequest(address);
+      expect(request.getEnvelope().toJson(), {
+        'Body': {
+          'SendShareNotificationRequest': {
+            '_jsns': 'urn:zimbraMail',
+            'a': address,
+          }
+        },
+      });
+    }));
+
+    test('Send verification code response', (() {
+      final json = {
+        'Body': {
+          'SendVerificationCodeResponse': {
+            '_jsns': 'urn:zimbraMail',
+          }
+        }
+      };
+      final envelope = SendVerificationCodeEnvelope.fromJson(json);
+      final response = envelope.sendVerificationCodeBody.sendVerificationCodeResponse;
+      expect(response, isNotNull);
+      expect(response, isA<SendVerificationCodeResponse>());
+    }));
+  }));
+}

c2ca96adc083d9ae80ef0077511b289e6f9c20a8
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-10-31 10:12:25 +0700

    Add set recover account test
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ test/mail/set_recovery_account_test.dart
@@ -0,0 +1,49 @@
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:zimbra_api/src/common/enum/channel.dart';
+import 'package:zimbra_api/src/common/enum/recovery_account_operation.dart';
+import 'package:zimbra_api/src/mail/message/set_recovery_account_envelope.dart';
+import 'package:zimbra_api/src/mail/message/set_recovery_account_request.dart';
+import 'package:zimbra_api/src/mail/message/set_recovery_account_response.dart';
+
+void main() {
+  final faker = Faker();
+
+  group('Set recover account tests', (() {
+    test('Set recover account request', (() {
+      final recoveryAccount = faker.internet.email();
+      final verificationCode = faker.lorem.word();
+
+      final request = SetRecoveryAccountRequest(
+        RecoveryAccountOperation.sendCode,
+        recoveryAccount,
+        verificationCode,
+        channel: Channel.email,
+      );
+      expect(request.getEnvelope().toJson(), {
+        'Body': {
+          'SetRecoveryAccountRequest': {
+            '_jsns': 'urn:zimbraMail',
+            'op': RecoveryAccountOperation.sendCode.name,
+            'recoveryAccount': recoveryAccount,
+            'recoveryAccountVerificationCode': verificationCode,
+            'channel': Channel.email.name,
+          }
+        },
+      });
+    }));
+
+    test('Set recover account response', (() {
+      final json = {
+        'Body': {
+          'SetRecoveryAccountResponse': {
+            '_jsns': 'urn:zimbraMail',
+          }
+        }
+      };
+      final envelope = SetRecoveryAccountEnvelope.fromJson(json);
+      expect(envelope.setRecoveryAccountBody.setRecoveryAccountResponse, isNotNull);
+      expect(envelope.setRecoveryAccountBody.setRecoveryAccountResponse, isA<SetRecoveryAccountResponse>());
+    }));
+  }));
+}
